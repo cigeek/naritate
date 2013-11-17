0fbb6bc2c46df54f586b4e7464144565d1e544ec
KOSK <tuna.mag@gmail.com>
2013-11-17 21:16:31 +0900

    fix migration file syntax error

--- db/migrate/20131117_create_table_foreign.rb
+++ db/migrate/20131117_create_table_foreign.rb
@@ -7,11 +7,25 @@ class CreateForeigntitles < ActiveRecord::Migration
       t.integer :times, :default => 0
       t.timestamps :added
     end
-
-    add_index :Foreigntitles, [:id, :title], :unique => true
   end
 
   def down
     drop_table :Foreigntitles
+  end
+end
+
+class CreateJapanesetitles < ActiveRecord::Migration
+  def up
+    create_table(:Japanesetitles, :id => false) do |t|
+      t.string :id
+      t.string :title
+      t.integer :favs, :default => 0
+      t.integer :times, :default => 0
+      t.timestamps :added
+    end
+  end
+
+  def down
+    drop_table :Japanesetitles
   end
 end
