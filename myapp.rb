93bc1a384af714086d9e98f1adb663a83dee5a92
KOSK <tuna.mag@gmail.com>
2013-12-23 11:02:46 +0900

    Add API function

--- myapp.rb
+++ myapp.rb
@@ -1,25 +1,33 @@
-# ライブラリ読み込み
+#=========#
+# 初期設定 #
+#=========#
+# ==ライブラリ読み込み== #
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'active_record'
 require 'sinatra/activerecord'
 
+# ==データベース設定== #
 # DB接続設定
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/dev.db')
 
-# テーブルをクラス化
+# DBテーブルをクラス化
 class Foreigntitle < ActiveRecord::Base
 end
 class Japanesetitle < ActiveRecord::Base
 end
 
+# ==定数の宣言== #
 # 一覧表示するレコードの件数
 MAX_TITLES = 18
 
 # ランキング表示するレコードの件数
 MAX_RANK = 5
 
+#========#
+# Web向け #
+#========#
 # トップ画面
 get '/' do
   @page_title = '旧作チェッカー『なりたてQ作』'
@@ -55,11 +63,11 @@ get '/ranking' do
   @fav_ranks = ActiveRecord::Base.connection.execute("select * from foreigntitles union select * from japanesetitles order by favs desc, created_at desc limit #{MAX_RANK};")
   # foreignTitles、japaneseTitlesテーブルの中で最もtimes数の多いレコード5件を取得
   @time_ranks = ActiveRecord::Base.connection.execute("select * from foreigntitles union select * from japanesetitles order by times desc, created_at desc limit #{MAX_RANK};")
-  
+
   erb :ranking
 end
 
-# 洋画ランキング 
+# 洋画ランキング
 get '/rankingfr' do
   @page_title = '洋画ランキング - 旧作チェッカー『なりたてQ作』'
 
@@ -74,7 +82,7 @@ end
 # 邦画ランキング
 get '/rankingjp' do
   @page_title = '邦画ランキング - 旧作チェッカー『なりたてQ作』'
-  
+
   # foreignTitlesテーブルの中で最もfav数の多いレコード5件を取得
   @fav_ranks = Japanesetitle.order("favs desc").limit(MAX_TITLES).order("created_at desc").limit(MAX_RANK).all
   # foreignTitlesテーブルの中で最もtimes数の多いレコード5件を取得
@@ -83,6 +91,47 @@ get '/rankingjp' do
   erb :rankingjp
 end
 
+#=====#
+# API #
+#=====#
+# 洋画タイトル一覧
+get '/foreign.json' do
+  content_type :json, :charset => 'utf-8'
+  titles = Foreigntitle.order("created_at desc").limit(MAX_TITLES).all
+  titles.to_json(:root => false)
+end
+
+# 邦画タイトル一覧
+get '/japanese.json' do
+  content_type :json, :charset => 'utf-8'
+  titles = Japanesetitle.order("created_at desc").limit(MAX_TITLES).all
+  titles.to_json(:root => false)
+end
+
+# 総合ランキング
+get '/ranking.json' do
+  content_type :json, :charset => 'utf-8'
+  titles = ActiveRecord::Base.connection.execute("select * from foreigntitles union select * from japanesetitles order by favs desc, created_at desc limit #{MAX_RANK};")
+  titles.to_json(:root => false)
+end
+
+# 洋画ランキング
+get '/rankingfr.json' do
+  content_type :json, :charset => 'utf-8'
+  titles = Foreigntitle.order("favs desc").limit(MAX_TITLES).order("created_at desc").limit(MAX_RANK).all
+  titles.to_json(:root => false)
+end
+
+# 邦画ランキング
+get '/rankingjp.json' do
+  content_type :json, :charset => 'utf-8'
+  titles = Japanesetitle.order("favs desc").limit(MAX_TITLES).order("created_at desc").limit(MAX_RANK).all
+  titles.to_json(:root => false)
+end
+
+#=========#
+# 投票機能 #
+#=========#
 # favs数のインクリメント処理
 post '/fav/:table' do
   # 洋画タイトル一覧からのpostだった場合
@@ -91,7 +140,7 @@ post '/fav/:table' do
     title.favs += 1
     title.save
 
-  # 邦画タイトル一覧からのpostだった場合  
+  # 邦画タイトル一覧からのpostだった場合
   elsif params[:table] == 'jp'
     title = Japanesetitle.find(params[:id])
     title.favs += 1
