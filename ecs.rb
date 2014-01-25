2fd8dd388ea1743b0b22a341c1b32ae19934f5ac
KOSK <tuna.mag@gmail.com>
2014-01-25 12:46:34 +0900

    fix .gitignore problem

--- ecs.rb
+++
@@ -1,7 +0,0 @@
-#== Amazon API環境設定 ==#
-# 接続設定
-Amazon::Ecs.options = {
-  :associate_tag => 'cigeek-22',
-  :AWS_access_key_id => 'AKIAJA6MN3AC3L2XZQJQ',
-  :AWS_secret_key => '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'
-}
