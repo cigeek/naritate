29ffe40b04f08380d7c1bc6a2314d06159b7b98d
KOSK <tuna.mag@gmail.com>
2014-02-11 22:33:58 +0900

    remove env.rb

--- myapp.rb
+++ myapp.rb
@@ -5,8 +5,22 @@ require 'sinatra/reloader' if development?
 require 'active_record'
 require 'sinatra/activerecord'
 
-#== 環境設定 ==#
-require './env'
+#== データベース環境設定 ==#
+# データベース接続
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/dev.db')
+
+# テーブルをクラス化
+class Foreigntitle < ActiveRecord::Base
+end
+class Japanesetitle < ActiveRecord::Base
+end
+
+#== 定数 ==#
+# 一覧表示するレコードの件数
+MAX_TITLES = 18
+
+# ランキング表示するレコードの件数
+MAX_RANK = 5
 
 #== Web ==#
 # トップ画面
--- naritate.rb
+++ naritate.rb
@@ -5,8 +5,23 @@ require 'open-uri'
 require 'nokogiri'
 require 'amazon/ecs'
 
-#== 環境設定 ==#
-require './env'
+#== データベース環境設定 ==#
+# データベース接続
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'sqlite3://localhost/dev.db')
+
+# テーブルをクラス化
+class Foreigntitle < ActiveRecord::Base
+end
+class Japanesetitle < ActiveRecord::Base
+end
+
+#== 定数 ==#
+# 一覧表示するレコードの件数
+MAX_TITLES = 18
+
+# ランキング表示するレコードの件数
+MAX_RANK = 5
+
 # AWS接続設定
 Amazon::Ecs.options = {
   :associate_tag => 'cigeek-22',
